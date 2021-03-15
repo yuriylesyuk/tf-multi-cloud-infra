78da53f1d27c59ebc1d1ea0dc833025b4ce01c5e
Yuriy Lesyuk <yuriylesyuk@gmail.com>
2021-03-15 21:54:44 +0000

    cloud accounts login checks

--- install-gcp-aws-az.sh
+++ install-gcp-aws-az.sh
@@ -2,12 +2,21 @@
 # Check we are logged in
 
 
+set -e
+
+# check we are logged in
+echo "Check if logged in gcloud: "
+gcloud compute instances list
+
+echo "Check if logged in aws: "
+aws sts get-caller-identity
+
+echo "Check if logged in az: "
+az account show
 
 
-az status
-
-
-
+echo "!!!!!!!!!"
+exit
 
 # Check required environment variables
 
